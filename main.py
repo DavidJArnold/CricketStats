ea5164c14e67a614517c92a4295f83bca8b1606d
David <david.14587@gmail.com>
2021-06-28 16:39:28 +1000

    Added per over summary function

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ import ruamel.yaml as yaml
 import glob
 from helper_functions import match_summary
 
+
+def count_wickets_and_extras(the_ball, wickets, noballs, wides) -> tuple[list, list, list]:
+    # the delivery information is the only key-value pair in the ball field
+    # the key is in the format 23.4, meaning 4th ball of the 24th over (0.1 is the first ball of the innings)
+    key = list(the_ball)[0]
+
+    # did a wicket fall?
+    if 'wicket' in the_ball[key]:
+        # add 1 to the count of wickets for the appropriate ball number
+        wickets[int(str(key).split(".")[-1]) - 1] += 1
+
+    # was there an extra?
+    if 'extras' in the_ball[key]:
+        if 'noballs' in the_ball[key]['extras']:
+            noballs[int(str(key).split(".")[-1]) - 1] += 1
+        if 'wides' in the_ball[key]['extras']:
+            wides[int(str(key).split(".")[-1]) - 1] += 1
+
+    return wickets, noballs, wides
+
+
 # initialise counting arrays for actions happening on specific balls
 ball_wickets = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ball_noballs = [0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -32,22 +53,9 @@ for idx, name in enumerate(files):
     # iterate through the innings' in the match
     for innings in list(test['innings']):
         # iterate through the balls in the innings
-        for balls in innings[list(innings)[0]]['deliveries']:
-            # the delivery information is the only key-value pair in the ball field
-            # the key is in the format 23.4, meaning 4th ball of the 24th over (0.1 is the first ball of the innings)
-            key = list(balls)[0]
-
-            # did a wicket fall?
-            if 'wicket' in balls[key]:
-                # add 1 to the count of wickets for the appropriate ball number
-                ball_wickets[int(str(key).split(".")[-1]) - 1] += 1
-
-            # was there an extra?
-            if 'extras' in balls[key]:
-                if 'noballs' in balls[key]['extras']:
-                    ball_noballs[int(str(key).split(".")[-1]) - 1] += 1
-                if 'wides' in balls[key]['extras']:
-                    ball_wides[int(str(key).split(".")[-1]) - 1] += 1
+        for ball in innings[list(innings)[0]]['deliveries']:
+            ball_wickets, ball_noballs, ball_wides = count_wickets_and_extras(
+                ball, ball_wickets, ball_noballs, ball_wides)
 
     # display current results
     print(ball_wickets)
